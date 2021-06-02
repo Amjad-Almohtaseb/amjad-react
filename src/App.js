bac2241292e9f5cc1341b08e72d46425e56eb5c9
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-06-02 11:46:46 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./components/Home.js";
+import Electronicshop from "./components/Electronicshop";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Electronicshop />
+    </div>
+  );
+}
+
+export default App;
